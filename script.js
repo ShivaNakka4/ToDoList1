bbe9ee3412ced00dadc99bc20368ab06fab35927
ShivaNakka4 <140834474+ShivaNakka4@users.noreply.github.com>
2023-07-31 12:32:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -479,8 +479,9 @@ function updateEvents(date) {
  function saveEvents() {
      localStorage.setItem("events", JSON.stringify(eventsArr));
  }
+
  function getEvents() {
-     if (localStorage.getItem("events" == null)) {
+     if (localStorage.getItem("events") == null) {
          return;
      }
      eventsArr.push(... JSON.parse(localStorage.getItem("events")));
